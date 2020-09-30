a932940f4d80262457e5b8e07e12e6dc5c0bdfd5
Daniel Pavela <daniel.pavela@gmail.com>
2020-09-30 18:42:58 +1000

    Add alias functionality

--- src/main.sh
+++ src/main.sh
@@ -1,18 +1,29 @@
 #!/usr/bin/env bash
 
+# Colours
+BLUE='\e[34m'
+BOLD='\e[1m'
+RESET='\033[0m'
+NC='\e[39m'
+GREEN='\e[32m'
+YELLOW='\e[33m'
+RED='\033[0;31m'
+
 case $1 in
 	"add")
-		echo $2
+		alias $2="cd $PWD"
+		echo -e "${BOLD}${GREEN}Alias added${NC}"
 		;;
 
 	"remove")
-		echo "remove"
+		unalias $2
+		echo -e "${BOLD}${GREEN}Alias removed${NC}"
 		;;
 
 	"list")
-		echo "list"
+		echo "Work in progress..."
 		;;
 
 	*)
-		echo "Command not found!"
+		echo -e "${RED}ERROR${NC}\nNo command found!"
 esac
